b457ad74b04e32e82beb303dc56975d4782a4031
Rabeendran <rajeevan@bluewin.ch>
2012-03-27 12:27:18 +0200

    Kapitel 7 - updates

--- depot/test/unit/product_test.rb
+++ depot/test/unit/product_test.rb
@@ -1,11 +1,3 @@
-#---
-# Excerpted from "Agile Web Development with Rails",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
-#---
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
@@ -19,17 +11,17 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product price must be positive" do
-    product = Product.new(title:       "My Book Title",
+    product = Product.new(title: "My Book Title",
                           description: "yyy",
-                          image_url:   "zzz.jpg")
+                          image_url: "zzz.jpg")
     product.price = -1
     assert product.invalid?
-    assert_equal "must be greater than or equal to 0.01", 
+    assert_equal "must be greater than or equal to 0.01",
       product.errors[:price].join('; ')
 
     product.price = 0
     assert product.invalid?
-    assert_equal "must be greater than or equal to 0.01", 
+    assert_equal "must be greater than or equal to 0.01",
       product.errors[:price].join('; ')
 
     product.price = 1
@@ -37,15 +29,15 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   def new_product(image_url)
-    Product.new(title:       "My Book Title",
+    Product.new(title: "My Book Title",
                 description: "yyy",
-                price:       1,
-                image_url:   image_url)
+                price: 1,
+                image_url: image_url)
   end
 
   test "image url" do
     ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
-             http://a.b.c/x/y/z/fred.gif }
+http://a.b.c/x/y/z/fred.gif }
     bad = %w{ fred.doc fred.gif/more fred.gif.more }
     
     ok.each do |name|
@@ -58,20 +50,20 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product is not valid without a unique title" do
-    product = Product.new(title:       products(:ruby).title,
-                          description: "yyy", 
-                          price:       1, 
-                          image_url:   "fred.gif")
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
 
     assert !product.save
     assert_equal "has already been taken", product.errors[:title].join('; ')
   end
 
   test "product is not valid without a unique title - i18n" do
-    product = Product.new(title:       products(:ruby).title,
-                          description: "yyy", 
-                          price:       1, 
-                          image_url:   "fred.gif")
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
 
     assert !product.save
     assert_equal I18n.translate('activerecord.errors.messages.taken'),
